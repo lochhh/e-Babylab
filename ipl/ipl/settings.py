5570b503f6b1d9082fce99a1de45ea6ee9f84db1
Changhuan Lo <thisismade@live.com>
2021-01-29 14:40:57 +0800

    Fix postgres + grappelli

--- ipl/ipl/settings.py
+++ ipl/ipl/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
