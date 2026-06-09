17d8df6eaa44f72529c42c4c58ef39909d47eb0e
lochhh <changhuan.lo@ucl.ac.uk>
2026-06-09 12:25:05 +0800

    Rewrite migration 0007 to use regex-based script tag patching
    - fixes CRLF line ending edge cases that caused type="module" to be missed on some records

--- src/experiments/migrations/0007_update_template_script_tags.py
+++ src/experiments/migrations/0007_update_template_script_tags.py
@@ -1,50 +1,53 @@
+import re
+
 from django.db import migrations
 
-
-REPLACEMENTS = [
-    (
-        "browser_check_page_tpl",
-        "<script src=\"{% static 'experiments/js/browser-check.js' %}\"></script>",
-        "<script type=\"module\" src=\"{% static 'experiments/js/browser-check.js' %}\"></script>",
-    ),
-    (
-        "webcam_check_page_tpl",
-        "<script src=\"{% static 'experiments/js/webcam-calibration.js' %}\"></script>",
-        "<script type=\"module\" src=\"{% static 'experiments/js/webcam-calibration.js' %}\"></script>",
-    ),
-    (
-        "microphone_check_page_tpl",
-        "<script src=\"{% static 'experiments/js/webcam-calibration.js' %}\"></script>",
-        "<script type=\"module\" src=\"{% static 'experiments/js/webcam-calibration.js' %}\"></script>",
-    ),
-    (
-        "thank_you_page_tpl",
-        "<script src=\"{% static 'experiments/js/endpage.js' %}\"></script>",
-        "<script type=\"module\" src=\"{% static 'experiments/js/endpage.js' %}\"></script>",
-    ),
-    (
-        "thank_you_abort_page_tpl",
-        "<script src=\"{% static 'experiments/js/endpage.js' %}\"></script>",
-        "<script type=\"module\" src=\"{% static 'experiments/js/endpage.js' %}\"></script>",
-    ),
-    (
-        "demographic_data_page_tpl",
-        "<script src=\"{% static 'experiments/js/recaptcha-handler.js' %}\"></script>",
-        "<script type=\"module\" src=\"{% static 'experiments/js/resolution.js' %}\"></script>\n"
-        "    <script type=\"module\" src=\"{% static 'experiments/js/recaptcha-handler.js' %}\"></script>",
-    ),
+# Script files that must be loaded as ES modules.
+# webgazer.min.js is a non-module vendor bundle and is intentionally excluded.
+MODULE_SCRIPTS = [
+    "experiments/js/experiment.js",
+    "experiments/js/webgazer-calibration.js",
+    "experiments/js/webcam-calibration.js",
+    "experiments/js/browser-check.js",
+    "experiments/js/endpage.js",
+    "experiments/js/recaptcha-handler.js",
+    "experiments/js/resolution.js",
 ]
 
-EXPERIMENT_PAGE_OLD = (
-    "<script src=\"{% static 'experiments/js/experiment.js' %}\"></script>\n"
-    "<script src=\"{% static 'experiments/js/webgazer.min.js' %}\"></script>\n"
-    "<script src=\"{% static 'experiments/js/webgazer-calibration.js' %}\"></script>"
-)
+TEMPLATE_FIELDS = [
+    "experiment_page_tpl",
+    "webcam_check_page_tpl",
+    "microphone_check_page_tpl",
+    "browser_check_page_tpl",
+    "thank_you_page_tpl",
+    "thank_you_abort_page_tpl",
+    "demographic_data_page_tpl",
+]
 
-EXPERIMENT_PAGE_NEW = (
-    "<script src=\"{% static 'experiments/js/webgazer.min.js' %}\"></script>\n"
-    "    <script type=\"module\" src=\"{% static 'experiments/js/experiment.js' %}\"></script>"
-)
+
+def _add_module_type(tpl):
+    """Add type="module" to script tags that load known ES module files."""
+    for path in MODULE_SCRIPTS:
+        # Match <script ...> tags containing this path but lacking type="module".
+        # The src attribute value may contain single quotes (e.g. {% static '...' %}),
+        # so we match the full tag with [^>]* and check for the path within it.
+        tpl = re.sub(
+            rf'(<script(?![^>]*type=["\']module["\'])[^>]*{re.escape(path)}[^>]*>)',
+            lambda m: m.group(0).replace("<script", '<script type="module"', 1),
+            tpl,
+        )
+    return tpl
+
+
+def _remove_module_type(tpl):
+    """Remove type="module" from script tags that load known ES module files."""
+    for path in MODULE_SCRIPTS:
+        tpl = re.sub(
+            rf'(<script type="module"[^>]*{re.escape(path)}[^>]*>)',
+            lambda m: m.group(0).replace(' type="module"', "", 1),
+            tpl,
+        )
+    return tpl
 
 
 def update_template_script_tags(apps, schema_editor):
@@ -53,35 +56,34 @@ def update_template_script_tags(apps, schema_editor):
     updated = []
     for exp in Experiment.objects.all():
         changed = False
-        for field, old, new in REPLACEMENTS:
-            value = getattr(exp, field)
-            if old in value:
-                setattr(exp, field, value.replace(old, new))
+        for field in TEMPLATE_FIELDS:
+            old = getattr(exp, field)
+            new = _add_module_type(old)
+            if new != old:
+                setattr(exp, field, new)
                 changed = True
-        value = exp.experiment_page_tpl
-        if EXPERIMENT_PAGE_OLD in value:
-            exp.experiment_page_tpl = value.replace(EXPERIMENT_PAGE_OLD, EXPERIMENT_PAGE_NEW)
-            changed = True
         if changed:
             updated.append(exp)
     if updated:
-        Experiment.objects.bulk_update(
-            updated,
-            [
-                "browser_check_page_tpl",
-                "webcam_check_page_tpl",
-                "microphone_check_page_tpl",
-                "thank_you_page_tpl",
-                "thank_you_abort_page_tpl",
-                "demographic_data_page_tpl",
-                "experiment_page_tpl",
-            ],
-        )
+        Experiment.objects.bulk_update(updated, TEMPLATE_FIELDS)
 
 
 def reverse_template_script_tags(apps, schema_editor):
-    """No-op reverse for template script tag migration."""
-    pass
+    """Revert Experiment template fields to non-module script tags."""
+    Experiment = apps.get_model("experiments", "Experiment")
+    updated = []
+    for exp in Experiment.objects.all():
+        changed = False
+        for field in TEMPLATE_FIELDS:
+            old = getattr(exp, field)
+            new = _remove_module_type(old)
+            if new != old:
+                setattr(exp, field, new)
+                changed = True
+        if changed:
+            updated.append(exp)
+    if updated:
+        Experiment.objects.bulk_update(updated, TEMPLATE_FIELDS)
 
 
 class Migration(migrations.Migration):
